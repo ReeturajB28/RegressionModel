70ccf653b3720bb1aa6892d4b29641f5a095142e
ReeturajB28 <reeturajb28@gmail.com>
2024-06-26 14:05:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request,render_template,jsonify
+
+app = Flask(__name__)
+
+@app.route("/home", methods=['GET','POST'])
+def main_page():
+    return render_template('index.html')
+
+@app.route('/math',methods=['POST'])
+def operation():
+    if request.method=='POST':
+        ops=request.form['operation']
+        a=int(request.form['num1'])
+        b=int(request.form['num2'])
+        if ops=='add':
+            results=a+b
+            return render_template('results.html',result=results)
+
+@app.route('/postman',methods=['POST'])
+def post_data():
+    if request.method=='POST':
+        ops=request.json['operation']
+        a=int(request.json['num1'])
+        b=int(request.json['num2'])
+        if ops=='add':
+            results=a+b
+            return jsonify(results)
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
